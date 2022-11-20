2f6d4599d4bd63a56fc19658f0b735c6f948b5fe
YUKINO <78239937+yukihooked@users.noreply.github.com>
2022-11-19 19:21:39 -0500

    Add files via upload

---
+++ alt_hasher.lua
@@ -0,0 +1,32 @@
+local local_player = game.Players.LocalPlayer
+
+if not isfile("alt_hashes.txt") then
+  writefile("alt_hashes.txt", "")
+end
+
+local found_hash = false
+local hash_list = readfile("alt_hashes.txt")
+local lines = hash_list:split("\n")
+local clock_offset
+
+for i,v in next, lines do
+  if found_hash then
+    break
+  end
+  
+  local line_split = v:split(":")
+  if string.find(v[1], tostring(local_player.UserId)) then
+    clock_offset = line_split[2]
+    found_hash = true
+  end
+end
+
+if not found_hash then
+  math.randomseed(tick())
+  clock_offset = math.random(1,200) + math.random(1,10000)/10000
+  appendfile("alt_hashes.txt", tostring(local_player.UserId)..":"..tostring(clock_offset).."\n")
+end
+
+local os_clock; os_clock = hookfunction(os.clock, function() 
+  return os_clock() + clock_offset
+end)
